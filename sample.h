11b80d4fc1612e92355381514f33730982d9b3bf
SinuSundaresan <sinusundaresan@yahoo.com>
2014-05-12 18:40:34 -0400

    myfirstcommit

---
+++ sample.h
@@ -0,0 +1,13 @@
+//
+//  sample.h
+//  sampleone1
+//
+//  Created by shankaar on 2014-05-12.
+//  Copyright (c) 2014 lasha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sample : UICollectionViewController
+
+@end
---
+++ sample.m
@@ -0,0 +1,49 @@
+//
+//  sample.m
+//  sampleone1
+//
+//  Created by shankaar on 2014-05-12.
+//  Copyright (c) 2014 lasha. All rights reserved.
+//
+
+#import "sample.h"
+
+@interface sample ()
+
+@end
+
+@implementation sample
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
